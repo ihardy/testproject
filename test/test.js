da186aa14253cbc7cecaf729e5202903a027ae3c
Ian Hardy <ian.c.hardy@gmail.com>
2019-02-25 12:09:29 -0500

    add failing test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#sum()', function() {
+    it('one and one should be two', function() {
+      assert.equal((1 + 2), 2);
+    });
+  });
+});
+
